2010fa1f8fbc13ebfec119cc9535fd4d186555a9
sleeperbus <sleeperbus@gmail.com>
2025-01-12 21:49:42 +0900

    * https://youtu.be/WT9-4DXUqsM?si=Z-C9okUGwxjuEt44&t=3648

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -1,10 +1,17 @@
 package com.plcoding.bookpedia
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreenRoot
+import com.plcoding.bookpedia.book.presentation.book_list.BookListViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-
+    BookListScreenRoot(
+        viewModel = remember { BookListViewModel() },
+        onBookClick = {
+        }
+    )
 }
