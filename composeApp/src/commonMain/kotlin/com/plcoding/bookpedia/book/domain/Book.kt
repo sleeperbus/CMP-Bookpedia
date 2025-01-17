29e1c735310a118e04f74de8bc5e7ef2ebb2404e
sleeperbus <sleeperbus@gmail.com>
2025-01-17 21:39:35 +0900

    * https://youtu.be/WT9-4DXUqsM?si=V4ejbMDuFE1P2jiz&t=4769

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/Book.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/Book.kt
@@ -4,7 +4,7 @@ data class Book(
     val id: String,
     val title: String,
     val imageUrl: String,
-    val author: List<String>,
+    val authors: List<String>,
     val description: String?,
     val languages: List<String>,
     val firstPublishYear: String?,
