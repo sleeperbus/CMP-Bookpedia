3cc3ffdc906316510dcc33d54332e93df96af7fc
sleeperbus <sleeperbus@gmail.com>
2025-01-17 23:32:42 +0900

    * https://youtu.be/WT9-4DXUqsM?si=01v9M2AGirfg_K5g&t=6305
    더미 북을 기기에 표시한다.

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Preview.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Preview.kt
@@ -7,6 +7,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
+import com.plcoding.bookpedia.book.presentation.book_list.BookListState
+import com.plcoding.bookpedia.book.presentation.book_list.books
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 
 @Preview
@@ -25,4 +28,16 @@ private fun BookSearchBarPreview() {
                 .fillMaxWidth()
         )
     }
+}
+
+
+@Preview
+@Composable
+private fun BookListScreenPreview() {
+    BookListScreen(
+        state = BookListState(
+            searchResults = books
+        ),
+        onAction = {}
+    )
 }
