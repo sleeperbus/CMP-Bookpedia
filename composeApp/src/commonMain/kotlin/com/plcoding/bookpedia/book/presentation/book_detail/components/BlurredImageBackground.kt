b214d0d7da31904c0bc8306492f71162872d82fd
sleeperbus <sleeperbus@gmail.com>
2025-02-09 20:31:58 +0900

    * https://youtu.be/WT9-4DXUqsM?si=DptkMw715XU1M5Tg&t=14721
    책의 세부 정보가 나온다.

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/components/BlurredImageBackground.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/components/BlurredImageBackground.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
@@ -123,7 +124,7 @@ fun BlurredImageBackground(
         ) {
             Spacer(modifier = Modifier.fillMaxHeight(0.15f))
             ElevatedCard(
-                modifier = Modifier.width(200.dp).aspectRatio(2 / 3f),
+                modifier = Modifier.height(230.dp).aspectRatio(2 / 3f),
                 shape = RoundedCornerShape(8.dp),
                 colors = CardDefaults.elevatedCardColors(
                     containerColor = Color.Transparent
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/components/BookChip.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/components/BookChip.kt
@@ -1,7 +1,10 @@
 package com.plcoding.bookpedia.book.presentation.book_detail.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,7 +23,7 @@ enum class ChipSize {
 fun BookChip(
     modifier: Modifier = Modifier,
     size: ChipSize = ChipSize.REGULAR,
-    chipContent: @Composable () -> Unit
+    chipContent: @Composable RowScope.() -> Unit
 ) {
     Box(
         modifier =modifier
@@ -38,7 +41,11 @@ fun BookChip(
             ),
         contentAlignment = Alignment.Center
     ) {
-        chipContent()
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            chipContent()
+        }
     }
-
 }
