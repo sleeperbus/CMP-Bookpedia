34fa3523f724c36a013e98ec36857ee6fed5cc86
sleeperbus <sleeperbus@gmail.com>
2025-02-22 22:54:31 +0900

    * desktop까지 개발 완료

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/app/App.kt
@@ -1,5 +1,7 @@
 package com.plcoding.bookpedia.app
 
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -33,7 +35,10 @@ fun App() {
             navigation<Route.BookGraph>(
                 startDestination = Route.BookList
             ) {
-                composable<Route.BookList> {
+                composable<Route.BookList>(
+                    exitTransition = { slideOutHorizontally() },
+                    popEnterTransition = { slideInHorizontally() }
+                ) {
                     val viewModel = koinViewModel<BookListViewModel>()
                     val selectedBookViewModel =
                         it.sharedKoinViewModel<SelectedBookViewModel>(navController)
@@ -48,7 +53,10 @@ fun App() {
                         }
                     )
                 }
-                composable<Route.BookDetail> {
+                composable<Route.BookDetail>(
+                    enterTransition = { slideInHorizontally{initialState -> initialState} },
+                    exitTransition = { slideOutHorizontally{initialState -> initialState} }
+                ) {
                     val selectedBookViewModel =
                         it.sharedKoinViewModel<SelectedBookViewModel>(navController)
                     val selectedBook by selectedBookViewModel.selectedBook.collectAsStateWithLifecycle()
